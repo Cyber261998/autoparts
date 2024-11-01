90d53510996eb1fce7084c230d3b10b326898883
zd261998 <zd261998@outlook.com>
2024-11-01 11:30:01 +0800

    chore:update cloudflare configuration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   output: 'export',  // 确保静态输出
   images: {
-    unoptimized: true  // Cloudflare Pages 需要这个设置
+    unoptimized: true,  // Cloudflare Pages 需要这个设置
+    loader: 'custom',
+    loaderFile: './image-loader.js',
   }
 }
 
